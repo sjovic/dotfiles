73c2245f25b4e466a6524c515cc8671b10c3c929
Strahinja Jovic <s.jovic@levi9.com>
2020-04-23 23:38:06 +0200

    modify xmonad keybindings

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -10,9 +10,8 @@ import XMonad.Prompt.Shell
 import XMonad.Prompt.AppLauncher as AL
 
 myManageHook = composeAll
-    [ className =? "Skype" --> doShift "4"
-    , className =? "Firefox" --> doShift "3"
-    , className =? "Zathura" --> doShift "4"
+    [
+    className =? "Zathura" --> doShift "4"
     ]
 
 main = do
@@ -24,21 +23,20 @@ main = do
         , layoutHook = avoidStruts $ layoutHook def
         , modMask = mod4Mask
         , startupHook = setWMName "LG3D"
+        , terminal = "uxterm"
         }
-        `additionalKeys`
-        [((mod4Mask, xK_b), sendMessage ToggleStruts),
-                     ((mod1Mask .|. controlMask, xK_x), shellPrompt def),
-                     ((mod4Mask, xK_F4), spawn "systemctl suspend"),
-                     ((mod4Mask .|. shiftMask, xK_F4), spawn "systemctl poweroff"),
-                     ((mod1Mask .|. controlMask, xK_e), spawn "emacs"),
-                     ((mod1Mask .|. controlMask, xK_b), spawn "firefox"),
-                     ((mod1Mask .|. controlMask, xK_c), spawn "google-chrome-stable"),
-                     -- ((mod1Mask .|. controlMask, xK_s), spawn "skypeforlinux"),
-                     ((mod1Mask .|. controlMask, xK_l), spawn "slock"),
-                     ((0, 0x1008ff11), spawn "amixer sset Master 5%-"),
-                     ((0, 0x1008ff13), spawn "amixer sset Master 5%+"),
-                     ((0, 0x1008ff12), spawn "amixer sset Master toggle")] --,
-                  -- ((mod4Mask, xK_x), AL.launchApp defaultXPConfig ">")]
+        `additionalKeys` [
+            ((mod4Mask, xK_b), sendMessage ToggleStruts),
+         -- ((mod1Mask .|. controlMask, xK_x), shellPrompt def),
+         -- ((mod4Mask, xK_F9), spawn "systemctl suspend"),
+         -- ((mod4Mask .|. shiftMask, xK_F9), spawn "systemctl poweroff"),
+            ((mod1Mask .|. mod4Mask, xK_e), spawn "emacs"),
+            ((mod1Mask .|. mod4Mask, xK_c), spawn "chromium-browser"),
+            ((mod4Mask, xK_l), spawn "xscreensaver-command --lock"),
+            ((0, 0x1008ff11), spawn "amixer -q -D pulse sset Master 5%-"),
+            ((0, 0x1008ff13), spawn "amixer -q -D pulse sset Master 5%+"),
+            ((0, 0x1008ff12), spawn "amixer -q -D pulse sset Master toggle")]
+         -- ((mod4Mask, xK_x), AL.launchApp defaultXPConfig ">")]
        `removeKeys` [(mod4Mask, xK_p)] -- remove dmenu launch keybinding to use projectile
 
 workspaces' = ["1", "2", "3", "4", "5"]
