492310cd0ca84c9444294401e87c4d2630b5e8e0
Strahinja Jovic <sjovic@yandex.com>
2023-03-31 00:14:31 +0200

    remove elisp custom functions

--- .config/emacs/custom-functions.el
+++ .config/emacs/custom-functions.el
@@ -47,23 +47,6 @@ If point was already at that position, move point to beginning of line."
       (message (format "selected %s" font-name))
       (set-face-attribute 'default nil :font font-name)))
 
-(defun fdx/rename-current-buffer-file ()
-  "Renames current buffer and file it is visiting."
-  (interactive)
-  (let ((name (buffer-name))
-        (filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (error "Buffer '%s' is not visiting a file!" name)
-      (let ((new-name (read-file-name "New name: " filename)))
-        (if (get-buffer new-name)
-            (error "A buffer named '%s' already exists!" new-name)
-          (rename-file filename new-name 1)
-          (rename-buffer new-name)
-          (set-visited-file-name new-name)
-          (set-buffer-modified-p nil)
-          (message "File '%s' successfully renamed to '%s'"
-                   name (file-name-nondirectory new-name)))))))
-
 (global-set-key (kbd "C-c r") 'fdx/rename-current-buffer-file)
 
 (defun fdx/delete-current-buffer-file ()
